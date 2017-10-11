78d2727a76cb2416f9bedd82b80280a0f5d724e0
lzping <lzping0000@163.com>
2017-10-11 10:45:00 +0800

    remove repetitive elements

--- department_student/department_student/generation.cpp
+++ department_student/department_student/generation.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>//赖志平
+#include <cstdlib>
 #include <fstream>
-#include<ctime>
+#include <ctime>
 #include <string>
+#include <set>
 #include "rapidjson/document.h"
 #include "rapidjson/prettywriter.h"
 #include "rapidjson/stringbuffer.h"
 #define N 20
 #define M 300
 using namespace std;
-using namespace rapidjson;  //引入rapidjson命名空
+using namespace rapidjson; 
 struct Department{
 	string department_no;
 	int member_limit;
@@ -26,7 +27,7 @@ struct Student{
 	int join_departments;
 }student[M];
 
-string generate_time()
+/*string generateTime()//产生随机的时间段
 {
 	int tmp;
 	string temp;
@@ -36,17 +37,64 @@ string generate_time()
 	string temp1 = "";
 	temp1 = temp1 + week[tmp] + ".";
 	tmp = 6 + rand() % 19;//产生6~24的随机数
+	int t = 1 + rand() % 4;//产生活动时长，1~4小时
 	_itoa(tmp, c, 10);
 	temp = c;
 	temp1 = temp1 + temp + ":00~";
-	int t = 1 + rand() % 4;//产生活动时长，1~4小时
 	tmp = tmp + t;
 	_itoa(tmp, c, 10);
 	temp = c;
 	temp1 = temp1 + temp + ":00";
 	return temp1;
 }
-string generate_tags()
+*/
+void generateTime(string* tim)//产生随机的时间段
+{
+	int tmp;
+	string week[7] = { "Mon.", "Tues.", "Thur.", "Wed.", "Fri.", "Sat.", "Sun." };
+	char c[10];
+	int tt = rand()%8;//产生一星期中有空的天数
+	if (tt == 0)
+		return;
+	int t = 0;
+	set<string>ss;
+	for(int i = 0; i < tt;i++)
+	{
+		tmp = rand() % 7; 
+		ss.insert(week[tmp]);
+
+	}
+	set<string>::iterator it;
+	for (it = ss.begin(); it != ss.end(); it++)
+	{
+		string temp1 = "";
+		string temp;
+		temp1 =temp1 + *it;
+		tmp = 6+rand() % 18;//产生一天中初始时间点(6-23之间)
+		_itoa(tmp, c, 10);
+		temp = c;
+		temp1 = temp1 + temp + ":00~";
+		while (tmp<24)
+		{
+			tmp = tmp + 1+rand() % 5;//生成结束时间
+			if (tmp>24)
+				break;
+			_itoa(tmp, c, 10);
+			temp = c;
+			temp1 = temp1 + temp + ":00";
+			tim[t++] = temp1;
+			temp1 = "";
+			tmp = tmp + 1+ rand() % 16;//生成一天内下一次初始时间点
+			if (tmp >= 24)
+				break;
+			_itoa(tmp, c, 10);
+			temp = c;
+			temp1 = temp1 + *it+ temp + ":00~";
+		}
+	}
+}
+
+string generateTags()//产生随机的tags
 {
 	//srand((unsigned)time(NULL));
 	string tag[20] = { "study", "film", "English", "music", "reading", "chess", "football", "dance", "programming", "basketball",
@@ -60,6 +108,7 @@ int main()
 {
 	int tmp;
 	char c[10];
+	set <string> strset;
 	srand((unsigned)time(NULL));
 	Document doc;
 	doc.SetObject();
@@ -71,13 +120,23 @@ int main()
 		Value obj(kObjectType);
 
 		Value free_time_array(kArrayType);
-		int k = 1 + rand() % 20;
+
+		generateTime(student[i].free_time);
+		int t = 0;
+		while (student[i].free_time[t] != ""&&t < 20)
+		{
+			temp_string.SetString(StringRef(student[i].free_time[t].c_str()));
+			free_time_array.PushBack(temp_string, allocator);
+			t++;
+		}
+/*		int k = rand() % 11;
 		for (int j = 0; j < k; j++)
 		{
-			student[i].free_time[j] = generate_time();
+			student[i].free_time[j] = generateTime();
 			temp_string.SetString(StringRef(student[i].free_time[j].c_str()));
 			free_time_array.PushBack(temp_string, allocator);
 		}
+	*/	
 		obj.AddMember("free_time", free_time_array, allocator);
 
 		tmp = 1 + rand() % 20;
@@ -94,7 +153,7 @@ int main()
 		obj.AddMember("student_no", temp_string, allocator);
 
 		Value applications_department_array(kArrayType);
-		k = 1 + rand() % 5;
+		int	k = 1 + rand() % 5;
 		for (int j = 0; j < k; j++)
 		{
 			tmp = rand() % 20 + 1;
@@ -118,9 +177,10 @@ int main()
 		obj.AddMember("applications_department", applications_department_array, allocator);
 
 		Value tag_array(kArrayType);
+		tmp = 0 + rand() % 7;
 		for (int j = 0; j < tmp; j++)
 		{
-			student[i].tags[j] = generate_tags();
+			student[i].tags[j] = generateTags();
 			temp_string.SetString(StringRef(student[i].tags[j].c_str()));
 			tag_array.PushBack(temp_string, allocator);
 		}
@@ -138,13 +198,22 @@ int main()
 		Value obj(kObjectType);
 
 		Value event_schedules_array(kArrayType);
-		int k = 1 + rand() % 20;
+		generateTime(department[i].event_schedules);
+		int t = 0;
+		while (department[i].event_schedules[t] != ""&&t < 20)
+		{
+			temp_string.SetString(StringRef(department[i].event_schedules[t].c_str()));
+			event_schedules_array.PushBack(temp_string, allocator);
+			t++;
+		}
+
+/*		int k =  rand() % 8;
 		for (int j = 0; j < k; j++)
 		{
-			department[i].event_schedules[j] = generate_time();
+			department[i].event_schedules[j] = generateTime();
 			temp_string.SetString(StringRef(department[i].event_schedules[j].c_str()));
 			event_schedules_array.PushBack(temp_string, allocator);
-		}
+		}*/
 		obj.AddMember("event_schedules", event_schedules_array, allocator);
 
 		department[i].member_limit = 10 + rand() % 6;
@@ -169,10 +238,10 @@ int main()
 
 
 		Value tag_array(kArrayType);
-		tmp = 1 + rand() % 20;
+		tmp = 1 + rand() % 10;
 		for (int j = 0; j < tmp; j++)
 		{
-			department[i].tags[j] = generate_tags();
+			department[i].tags[j] = generateTags();
 			temp_string.SetString(StringRef(department[i].tags[j].c_str()));
 			tag_array.PushBack(temp_string, allocator);
 		}
